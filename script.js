1a6d43ef98ebde0423d7fbcd831784bad922f5dd
Jeff Brubaker <jtbrubak1@gmail.com>
2017-11-20 01:29:29 -0500

    Finish img loaders

---
+++ script.js
@@ -0,0 +1,29 @@
+images = ['./guy_fieri.jpg', './cool_dude.jpg'];
+image_count = 0;
+
+function inView(img_container) {
+  const rect = img_container.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+function loadImage(img_container) {
+  img_container.className = 'img-container-loaded';
+  var img = document.createElement('img');
+  img.setAttribute('src', images[0]);
+  images.shift();
+  img_container.appendChild(img);
+}
+
+function checkImages() {
+  const img_container = document.getElementsByClassName('image-container')[0];
+  if (inView(img_container)) { loadImage(img_container); }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  interval = setInterval(() => checkImages, 500);
+})
